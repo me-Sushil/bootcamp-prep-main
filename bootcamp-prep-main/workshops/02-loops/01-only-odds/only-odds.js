7f52017c333e2922f676cce12ad774b56ae8d8f0
me-Sushil <girbahadurbk123@gmail.com>
2025-03-31 20:28:45 +0545

    commit only-odds

--- bootcamp-prep-main/workshops/02-loops/01-only-odds/only-odds.js
+++ bootcamp-prep-main/workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,23 @@
-// YOUR CODE BELOW
+const onlyOdds = (num) => {
+    let sum = 0;
+  
+    for (let i = num; i >= 1; i--) {
+        if (i % 2 === 1) {
+          sum += i;
+      }
+    }
+      return sum;
+  }
+    function onlyOdds2(num) {
+    let sum = 0;
+      while (num >= 1) {
+        if (isOdd(num)) {
+        sum += num;
+      }
+        num--;
+    }
+    return sum;
+      function isOdd(num) {
+      return num % 2 === 1;
+    }
+  }
