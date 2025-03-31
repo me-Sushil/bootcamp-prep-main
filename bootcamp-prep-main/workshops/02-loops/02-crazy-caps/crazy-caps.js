f5a4080f51ea3101c87b3d1d5591cd9251ee8eac
me-Sushil <girbahadurbk123@gmail.com>
2025-03-31 22:29:39 +0545

    Crazy-Caps Commit

--- bootcamp-prep-main/workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ bootcamp-prep-main/workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,16 @@
-// YOUR CODE BELOW
+const crazyCaps= (mainString) => {
+    let crazyString = '';
+  
+    for (let i = 0; i < mainString.length; i++) {
+        let char = mainString[i];
+  
+      if (i % 2 === 0) {
+          crazyString += char;
+      }
+        else {
+          char = char.toUpperCase();
+          crazyString += char;
+      }
+    }
+      return crazyString;
+  }
