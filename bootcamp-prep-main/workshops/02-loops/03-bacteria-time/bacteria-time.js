a776557aa728541628cda0502b0f7d6b027b0e0e
me-Sushil <girbahadurbk123@gmail.com>
2025-03-31 22:46:58 +0545

    bacteriaTime commit

--- bootcamp-prep-main/workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ bootcamp-prep-main/workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,15 @@
-// YOUR CODE BELOW
+const bacteriaTime = (currentNum, targetNum) => {
+
+    if (targetNum < currentNum) {
+      return 'targetNum must be larger than currentNum';
+    }
+  
+    let numMinutes = 0;
+  
+    while (currentNum < targetNum) {
+        currentNum *= 2;
+        numMinutes += 20;
+    }
+  
+    return numMinutes;
+  }
