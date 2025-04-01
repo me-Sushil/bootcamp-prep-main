0aa429c88d360f27b87c8d51045774b54520ec05
me-Sushil <girbahadurbk123@gmail.com>
2025-04-01 17:57:26 +0545

    my-slice commit

--- bootcamp-prep-main/workshops/02-loops/05-my-slice/my-slice.js
+++ bootcamp-prep-main/workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+const mySlice = (originalString, startIdx, endIdx) => {
+
+    if (startIdx === undefined) {
+      return originalString;
+    }
+  
+    if (endIdx === undefined) {
+      endIdx = originalString.length;
+    }
+  
+    let slicedString = '';
+  
+    for (let i = startIdx; i < endIdx; i++) {
+  
+      let char = originalString[i];
+  
+      slicedString += char;
+    }
+  
+    return slicedString;
+  }
