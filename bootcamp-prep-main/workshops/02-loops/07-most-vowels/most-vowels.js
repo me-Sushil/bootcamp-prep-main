7feb3f55e306e3de8216d127a90a9bedd949a707
me-Sushil <girbahadurbk123@gmail.com>
2025-04-01 19:39:44 +0545

    mostvowel commit

--- bootcamp-prep-main/workshops/02-loops/07-most-vowels/most-vowels.js
+++ bootcamp-prep-main/workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,1 +1,38 @@
-// YOUR CODE BELOW
+const mostVowels = (sentence) => {
+    let currentWord = '';
+    let currentVowelCount = 0;
+  
+    let maxWord = '';
+    let maxVowelCount = 0;
+  
+    for (let i = 0; i < sentence.length; i++) {
+  
+      let char = sentence[i];
+  
+      if (char !== ' ' && i !== sentence.length - 1) {
+          currentWord += char;
+          if (isVowel(char)) {
+            currentVowelCount += 1;
+        }
+      }
+        else {
+          if (currentVowelCount > maxVowelCount) {
+            maxVowelCount = currentVowelCount;
+            maxWord = currentWord;
+        }
+          currentWord = '';
+        currentVowelCount = 0;
+      }
+    }
+      return maxWord;
+      function isVowel(char) {
+      let vowels = 'aeiouAEIOU';
+  
+      if (vowels.indexOf(char) >= 0) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+  }
