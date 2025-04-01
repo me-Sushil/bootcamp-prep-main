91ebeec2c1818f3551a9ec8645e33b94a32395cf
me-Sushil <girbahadurbk123@gmail.com>
2025-04-01 17:05:51 +0545

    exponentiate commit

--- bootcamp-prep-main/workshops/02-loops/04-exponentiate/exponentiate.js
+++ bootcamp-prep-main/workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,27 @@
 // YOUR CODE BELOW
+const exponentiate = (base, power) =>{
+
+    let result = 1;
+  
+    for (let i = 0; i < power; i++) {
+      result *= base;
+    }
+  
+    return result;
+  }
+  
+// alternate solution with a while loop
+//   function exponentiate(base, power) {
+//     if (power === 0) {
+//       return 1;
+//     }
+  
+//     let result = base;
+  
+//     while (power > 1) {
+//       result *= base;
+//       power--;
+//     }
+  
+//     return result;
+//   }
