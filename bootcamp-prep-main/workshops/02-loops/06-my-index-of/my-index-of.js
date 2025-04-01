1e399fff088671bce183eb593fb6ba34c5a7d2e5
me-Sushil <girbahadurbk123@gmail.com>
2025-04-01 18:15:14 +0545

    my-index commit

--- bootcamp-prep-main/workshops/02-loops/06-my-index-of/my-index-of.js
+++ bootcamp-prep-main/workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,32 @@
 // YOUR CODE BELOW
+function myIndexOf(source, searchValue, startIdx) {
+
+    if (startIdx === undefined) {
+      startIdx = 0;
+    }
+  
+    for (let i = startIdx; i <= source.length - searchValue.length; i++) {
+  
+      let substring = source.slice(i, i + searchValue.length);
+  
+      if (substring === searchValue) {
+  
+        return i;
+      }
+    }
+  
+    return -1;
+  }
+  
+// alternate solution that uses ES6 default parameters to handle startIdx
+//   function myIndexOf2(source, searchValue, startIdx=0) {
+//     for (let i = startIdx; i <= source.length - searchValue.length; i++) {
+//       let substring = source.slice(i, i + searchValue.length);
+  
+//       if (substring === searchValue) {
+//         return i;
+//       }
+//     }
+  
+//     return -1;
+//   }
