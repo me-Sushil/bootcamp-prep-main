8fb0f7a373cf151fe65a2eda29ff807ad5d2c9df
me-Sushil <girbahadurbk123@gmail.com>
2025-04-06 20:19:30 +0545

    ZeroDarkThirty Commit

--- bootcamp-prep-main/workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ bootcamp-prep-main/workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+const zeroDarkThirty = (num) =>{
+
+   if(num === 0){
+    return NaN;
+   }
+
+  num = String(num);
+
+  let newValue = '';
+
+  for (let i = 0; i < num.length; i++) {
+    let currentChar = num[i];
+
+    if (currentChar !== '0') {
+      newValue += currentChar;
+    }
+  }
+
+  return parseFloat(newValue);
+}
