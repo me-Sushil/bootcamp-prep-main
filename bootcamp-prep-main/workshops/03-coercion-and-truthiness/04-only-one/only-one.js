530930b9de16e70c6dc925b12a22a7f00cd23c49
me-Sushil <girbahadurbk123@gmail.com>
2025-04-06 19:09:58 +0545

    onlyone Commit

--- bootcamp-prep-main/workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ bootcamp-prep-main/workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,33 @@
 // YOUR CODE BELOW
+const onlyOne = (value1, value2, value3) =>{
+if(!!value1 && !value2 && !value3){
+    return true;
+}
+if(!value1 && !!value2 && !value3){
+    return true;
+}
+if(!value1 && !value2 && !!value3){
+    return true;
+}
+if(!value1 && !value2 && !value3){
+    return false;
+}
+if(!value1 && !!value2 && !!value3){
+    return false;
+}
+if(!!value1 && !value2 && !!value3){
+    return false;
+}
+if(!!value1 && !!value2 && !value3){
+    return false;
+}
+if(!!value1 && !!value2 && !!value3){
+    return false;
+}
+
+}
+// returns false if all of the values are falsey
+// returns false if only the first value is falsey
+// returns false if only the second value is falsey
+// returns false if only the third value is falsey
+// returns false if all of the values are truthy
