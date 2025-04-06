fe9b62d50c6eaa0f91b9e3270d87ff09b9ec7de9
me-Sushil <girbahadurbk123@gmail.com>
2025-04-06 18:47:28 +0545

    update

--- bootcamp-prep-main/workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
+++ bootcamp-prep-main/workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
@@ -1,1 +1,16 @@
-// YOUR CODE BELOW
+const howEqual = (value1, value2) => {
+
+    if (value1 === value2) {
+      return 'strictly';
+    }
+  
+    else if (value1 == value2) {
+      return 'loosely';
+    }
+  
+    else {
+      return 'not equal';
+    }
+  }
+  
+ 
--- bootcamp-prep-main/workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ bootcamp-prep-main/workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,1 +1,27 @@
 // YOUR CODE BELOW
+const myOr = (value1, value2, value3) =>{
+    if(!!value1){
+        return value1;
+    }
+    if(!!value2){
+        return value2;
+    }
+    else{
+        return value3;
+    }
+
+}
+const myAnd = ( value4, value5, value6) =>{
+    if(!value4){
+        return value4;
+    }
+    if(!value5){
+        return value5;
+    }
+    if(!value6){
+        return value6;
+    }
+    else{
+        return value6;
+    }
+}
