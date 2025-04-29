780ec519328fe1f7b46bb97609da4be2619ff4ad
me-Sushil <girbahadurbk123@gmail.com>
2025-04-29 14:45:54 +0545

    Array commit

--- bootcamp-prep-main/workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ bootcamp-prep-main/workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,88 @@
-// YOUR CODE BELOW
+// let names = ["Sushil", "Sushma", "Arjun", "Sagar"];
+// console.log(names);
+// console.log(Array.isArray(names));
+
+// let names = ["Sushil", "Sushma", "GS", "Sworag"];
+// console.log(names [0]);
+// names [0] ="Sagar";
+// console.log(names);
+// console.log(names.length);
+// console.log(names[names.length - 1]);
+
+// //////////////////////
+// let Addname = names.push("Arjun");
+// console.log(names);
+
+// ///////////////////////////
+// let latestAdd = names.pop();
+// console.log(names);
+
+// ////////////////////
+// let firstELRemove = names.shift();
+// console.log("shift name",names);
+
+// ////////////////////
+// names.unshift("Samir");
+// console.log("unshift name", names);
+
+// //////////////////
+// console.log(names.indexOf("GS"));
+
+
+/////////////////
+// let person = "Samir";
+// if(names.indexOf(person)> -1){
+//     console.log("Person Found");
+// }else{
+//     console.log("Person Not Found");
+// }
+
+// /////////////////
+// console.log(names);
+// console.log(names.slice(0,1));
+// console.log(names.slice(0,2));
+// console.log(names.slice(1,2));
+// console.log(names.slice(1));
+// console.log(names);
+
+// let namesCopy = names.slice();
+// namesCopy[0]="Aryan";
+// console.log(names);
+// console.log(namesCopy);
+
+
+
+// include method - check if a value is in array 
+// console.log(names.includes("GS"));
+// console.log(names.includes("devid"));
+
+
+// reverse method
+
+// console.log(names);
+// names.reverse();
+// console.log(names);
+// console.log(names[0]);
+
+
+
+
+
+function oddCouple(nums) {
+
+    let oddNums = [];
+      for (let i = 0 ; i < nums.length; i++) {
+       let currentNum = nums[i];
+  
+      if (currentNum % 2) {
+          oddNums.push(currentNum);
+  
+        if (oddNums.length === 2) {
+  
+          return oddNums;
+        }
+      }
+    }
+  
+    return oddNums;
+  }
--- bootcamp-prep-main/workshops/05-arrays-i/02-my-includes/my-includes.js
+++ bootcamp-prep-main/workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,1 +1,9 @@
-// YOUR CODE BELOW
+function myIncludes(array, searchValue) {
+    for (let i = 0; i < array.length; i++) {
+        let element = array[i];
+        if (element === searchValue) {
+          return true;
+      }
+    }
+    return false;
+  }
