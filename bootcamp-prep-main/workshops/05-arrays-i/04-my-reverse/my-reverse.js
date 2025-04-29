e69a43799505fbec67a18fa6f7928f736d6d7df8
me-Sushil <girbahadurbk123@gmail.com>
2025-04-29 20:10:20 +0545

    Reverse and unshift commit

--- bootcamp-prep-main/workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ bootcamp-prep-main/workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,9 @@
-// YOUR CODE BELOW
+function myReverse(originalArray) {
+    let reversedArray = [];
+      for (let i = originalArray.length - 1; i >= 0; i--) {
+        let element = originalArray[i];
+        reversedArray.push(element);
+    }
+      return reversedArray
+  }
+  
--- bootcamp-prep-main/workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ bootcamp-prep-main/workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,1 +1,9 @@
-// YOUR CODE BELOW
+function myUnshift(originalArray, value) {
+
+    let newArray = [value];
+    for (let i = 0; i < originalArray.length; i++) {
+        let element = originalArray[i];
+      newArray.push(element);
+    }
+      return newArray;
+}
