99be9861127d8c36f96dac0b8b3199dec399d218
me-Sushil <girbahadurbk123@gmail.com>
2025-04-29 20:01:31 +0545

    last index first commit

--- bootcamp-prep-main/workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ bootcamp-prep-main/workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,13 @@
-// YOUR CODE BELOW
+function myLastIndexOf(array, searchValue, startIdx) {
+
+    if (startIdx === undefined) {
+      startIdx = array.length - 1;
+    }
+      for (let i = startIdx; i >= 0; i--) {
+        let element = array[i];
+        if (element === searchValue) {
+          return i;
+      }
+    }
+      return -1;
+  }
