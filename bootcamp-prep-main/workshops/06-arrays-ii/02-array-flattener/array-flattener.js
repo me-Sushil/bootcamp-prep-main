550028933e1fe880b85359d6237600852ed922f5
me-Sushil <girbahadurbk123@gmail.com>
2025-05-03 18:15:35 +0545

    array flattener commit

--- bootcamp-prep-main/workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+function arrayFlattener(tDArray) {
+    let twoDArray= [];
+
+    for(let i=0; i<tDArray.length; i++){
+        let element = tDArray[i];
+
+        if(Array.isArray(element)){
+            for(let j=0; j<element.length; j++){
+                let innerElement = element[j];
+
+                twoDArray.push(innerElement);
+            }
+        }else{
+            twoDArray.push(element);
+        }
+    }
+
+    return twoDArray;
+}
+
+const twoDArray = arrayFlattener([1,2,[3,4],5]);
+console.log(twoDArray);
