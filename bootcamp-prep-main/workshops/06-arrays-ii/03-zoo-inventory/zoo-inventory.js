d0c51864457198ee2790badc69866a3c5e99a038
me-Sushil <girbahadurbk123@gmail.com>
2025-05-03 20:22:25 +0545

    zoo inventory commit

--- bootcamp-prep-main/workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+function zooInventory(zoo){
+    let sentences = [];
+
+    for(let i=0; i<zoo.length; i++){
+        let animal = zoo[i];
+
+        let name = animal[0];
+        let specis = animal[1][0];
+        let age = animal[1][1]
+
+       let sentencess = `${name} the ${specis} is ${age}.`;
+       sentences.push(sentencess);
+    }
+    return sentences;
+
+}
+const sentences = zooInventory( [
+    ['King Kong', ['gorilla', 42]],
+    ['Nemo', ['fish', 5]],
+    ['Punxsutawney Phil', ['groundhog', 11]]
+  ]);
+  console.log(sentences);
