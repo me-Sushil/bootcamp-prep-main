2fd5d926770c86f554e66665dd229e9f919053b3
me-Sushil <girbahadurbk123@gmail.com>
2025-05-06 14:10:31 +0545

    make grid commit

--- bootcamp-prep-main/workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,12 @@
-// YOUR CODE BELOW
+
+function makeGrid(numColumns, numRows) {
+    let grid = [];
+      for (let i = 0; i < numRows; i++) {
+        let row = [];
+      for (let j = 0; j < numColumns; j++) {
+        row.push(j + 1);
+      }
+        grid.push(row);
+    }
+      return grid;
+  }
