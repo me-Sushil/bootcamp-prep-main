6019cab8ea7347022c31186ab3dc0a1076d2c457
me-Sushil <girbahadurbk123@gmail.com>
2025-05-06 14:22:06 +0545

    remove colums commit

--- bootcamp-prep-main/workshops/06-arrays-ii/05-remove-columns/remove-columns.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/05-remove-columns/remove-columns.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+const removeColumns =(originalGrid, numColums)=>{
+    const removeGrid =[];
+
+    for(let i=0; i<originalGrid.length; i++){
+        const currentRow = originalGrid[i];
+        removeGrid.push(currentRow.slice(0, currentRow.length - numColums));
+    }
+ return removeGrid;
+}
