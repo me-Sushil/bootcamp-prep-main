5aaf8b9ddb17d82181f53477d016ac302a93a9b8
me-Sushil <girbahadurbk123@gmail.com>
2025-05-07 21:48:51 +0545

    my-join commit

--- bootcamp-prep-main/workshops/06-arrays-ii/06-my-join/my-join.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+const myJoin = (array, separator) => {
+
+    if(separator === undefined){
+        separator = ',';
+    }
+
+    let setString = [];
+
+    for( let i=0; i<array.length; i++){
+        let element = array[i];
+
+        if (i !== 0) {
+            setString += separator;
+          }
+          if (element !== undefined && element !== null) {
+                  setString += element;
+          }
+        }
+      
+        return setString;
+      }
