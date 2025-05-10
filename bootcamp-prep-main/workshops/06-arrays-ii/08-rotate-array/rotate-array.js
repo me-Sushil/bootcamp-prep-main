2221829254886c07e76cdf5f354968f70b34a5af
me-Sushil <girbahadurbk123@gmail.com>
2025-05-11 00:18:23 +0545

    rotate array commit

--- bootcamp-prep-main/workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+const rotateArray = ( originalArray, rotateNum ) =>{
+    const direction = rotateNum >= 0 ? 'right' : 'left';
+
+    const rotatedArray = originalArray.slice();
+
+    for ( let i=0; i< Math.abs(rotateNum) % originalArray.length; i++){
+        if(direction === 'right'){
+            rotatedArray.unshift(rotatedArray.pop());
+        }else{
+            rotatedArray.push(rotatedArray.shift());
+        }
+    }
+    return rotatedArray;
+}
