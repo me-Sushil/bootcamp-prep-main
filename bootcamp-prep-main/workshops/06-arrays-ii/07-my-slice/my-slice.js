24b7ab3a97bba8b4a9e3c7a2c9343e73a1647850
me-Sushil <girbahadurbk123@gmail.com>
2025-05-10 20:35:22 +0545

    mySlice commit

--- bootcamp-prep-main/workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+const mySlice = (originalArray, startIdx, endIdx) => {
+    
+  if (startIdx === undefined) {
+    startIdx = 0;
+  } else if (startIdx < 0) {
+    startIdx = originalArray.length + startIdx;
+  }
+
+  if (endIdx === undefined) {
+    endIdx = originalArray.length;
+  } else if (endIdx < 0) {
+    endIdx = originalArray.length + endIdx;
+  }
+
+  let arrayCopy = [];
+
+  for (let i = startIdx; i < endIdx; i++) {
+    let element = originalArray[i];
+    arrayCopy.push(element);
+  }
+
+  return arrayCopy;
+};
