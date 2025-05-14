aa7e9d3ec86cec2b051af60c74504ce453d84895
me-Sushil <girbahadurbk123@gmail.com>
2025-05-14 22:41:05 +0545

    last-friday-night-commit

--- bootcamp-prep-main/workshops/07-objects/01-last-friday-night/last-friday-night.js
+++ bootcamp-prep-main/workshops/07-objects/01-last-friday-night/last-friday-night.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+const lastFridayNight = (transactions) =>{
+let total = 0;
+for (let i = 0; i < transactions.length; i++) {
+    let transaction = transactions[i];
+    total += transaction.amount;
+  }
+ return total;
+}
