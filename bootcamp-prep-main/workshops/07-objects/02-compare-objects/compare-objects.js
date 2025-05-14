2025fb7e85a23e1ba00ddf7a2c12765993203a4c
me-Sushil <girbahadurbk123@gmail.com>
2025-05-14 22:54:36 +0545

    compare object commit

--- bootcamp-prep-main/workshops/07-objects/02-compare-objects/compare-objects.js
+++ bootcamp-prep-main/workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+const compareObjects = (obj1, obj2) =>{
+
+    for(let key in obj1){
+        if(obj1[key] !== obj2[key]){
+            return false;
+        }
+    }
+
+    for(let key in obj2){
+        if(obj1[key] !== obj2[key]){
+            return false;
+        }
+    }
+
+    return true;
+}
