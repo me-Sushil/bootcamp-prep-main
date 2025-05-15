0f9b594312c64ff40174cba75471097f681b0b06
me-Sushil <girbahadurbk123@gmail.com>
2025-05-15 22:35:03 +0545

    frequency analysis

--- bootcamp-prep-main/workshops/07-objects/05-frequency-analysis/README.md
+++ bootcamp-prep-main/workshops/07-objects/05-frequency-analysis/README.md
@@ -1,7 +1,6 @@
 ### Frequency Analysis
 
-Define a function `frequencyAnalysis` that accepts a string of lower-case
-letters.
+Define a function `frequencyAnalysis` that accepts a string of lower-case letters.
 
 `frequencyAnalysis` should return an object containing the count for each letter
 in the string.
--- bootcamp-prep-main/workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ bootcamp-prep-main/workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+const frequencyAnalysis =(string)=>{
+   let charFrequency = {};
+
+   for(let i = 0; i < string.length; i++){
+    let char = string[i];
+
+    if(!charFrequency[char]){
+        charFrequency[char]=1;
+    }else{
+        charFrequency[char]++;
+    }
+   }
+
+    return  charFrequency;
+}
