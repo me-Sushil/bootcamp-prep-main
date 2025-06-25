59587e5b75d61b95f77c57cfe0bdd9942fb01ade
me-Sushil <girbahadurbk123@gmail.com>
2025-06-25 21:05:29 +0545

    taco-cat-inc commit

--- bootcamp-prep-main/workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
+++ bootcamp-prep-main/workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
@@ -19,18 +19,50 @@ let tacoCatInc = {
     'treat dust': {cost: 0.1, quantity: 100}
   },
 
+  currentInventory: function(){
+    let sum = 0;
+    const ourInventory = [
+      this.gourmetShell,
+      this.gourmetFishFilling,
+      this.gourmetVeggie,
+      this.gourmetSeasoning,
+    ];
+
+    for(let i=0; i<ourInventory.length; ++i){
+      const inventoryItem = ourInventory[i];
+
+      const subItemValues = Object.values(inventoryItem);
+      
+      for(let j=0; j<subItemValues.length; ++j){
+        const {cost, quantity} = subItemValues[j];
+
+      const  itemValue = cost * quantity;
+
+      sum += itemValue;
+      }
+    }
+    return sum;
+  },
+
+  sale: function(order){
+    let totalSale = 0;
+
+    const orderItem = Object.entries(order);
+    for(let i=0; i<orderItem.length; ++i){
+      const [itemCategory, itemName] = orderItem[i];
+
+      const itemValues = this[itemCategory][itemName];
+      if(itemValues.quantity > 1){
+        totalSale += itemValues.cost;
+
+        this[itemCategory][itemName].quantity -= 1;
+      }
+    }
+    this.cash += totalSale;
+    return totalSale;
+  },
+
   cash: 0
 };
 
 // YOUR CODE BELOW
-
-let catstocas = {
-  currentInventory : function (){
-  },
-  sale : function(order){
-
-  },
-}
-let order = {
-  price : price,
-}
