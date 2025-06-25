d56497a5ae6696f1c8553ad03b57d0daafea81e3
me-Sushil <girbahadurbk123@gmail.com>
2025-06-25 15:20:25 +0545

    call them all commit

--- bootcamp-prep-main/workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ bootcamp-prep-main/workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function callThemAll(obj, value){
+    let returnedArray = [];
+
+    let valueOfObj = Object.values(obj);
+    for(let i=0; i<valueOfObj.length; ++i){
+        let currentValuesOfFun = valueOfObj[i];
+
+        if(typeof currentValuesOfFun === 'function'){
+            let resultOfFun = currentValuesOfFun(value);
+
+            returnedArray.push(resultOfFun);
+        }
+    }
+    return returnedArray;
+
+}
