9e58402511991306524c6272758c2c0e16251829
me-Sushil <girbahadurbk123@gmail.com>
2025-06-25 13:01:26 +0545

    say your name

--- bootcamp-prep-main/workshops/08-object-methods/02-say-your-name/say-your-name.js
+++ bootcamp-prep-main/workshops/08-object-methods/02-say-your-name/say-your-name.js
@@ -1,1 +1,12 @@
-// YOUR CODE BELOW
+let me2 = {
+    name : "Tarana",
+    getGreeting: function(anObj){
+        return "Hi " + anObj.name + ", my name is " + this.name + ".";
+    }
+}
+// let you = {
+//     name:"Sushil",
+//     }
+// console.log(me2.name); // 'Tarana'
+// console.log(you.name); // 'Sushil'
+// console.log(me2.getGreeting(you)); // => 'Hi Sushil, my name is Tarana.'
