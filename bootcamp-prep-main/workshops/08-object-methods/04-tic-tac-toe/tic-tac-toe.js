2c27aa7ff00dbc17fc80ba8c5e89c2a777ac0286
me-Sushil <girbahadurbk123@gmail.com>
2025-06-25 18:22:44 +0545

    tic-tac-toe

--- bootcamp-prep-main/workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ bootcamp-prep-main/workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+let ticTacToe = {
+    board : [[null, null, null],
+             [null, null, null],
+             [null, null, null]],
+    move : function (character, x, y){
+        if(this.board[y][x] === null){
+            this.board[y][x] = character;
+        }
+        return this.board;
+    },
+    clear : function (){
+        this.board = [[null, null, null],
+             [null, null, null],
+             [null, null, null]];
+    
+    return this.board;
+    }
+};
