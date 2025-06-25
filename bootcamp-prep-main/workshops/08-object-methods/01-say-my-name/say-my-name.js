4dbb197b42d066180d6326879cf43a9b32551d22
me-Sushil <girbahadurbk123@gmail.com>
2025-06-25 12:00:41 +0545

    method say name

--- bootcamp-prep-main/workshops/08-object-methods/01-say-my-name/say-my-name.js
+++ bootcamp-prep-main/workshops/08-object-methods/01-say-my-name/say-my-name.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+const me = {
+    name : "Kat",
+    getGreeting : function(){
+     return `Hi, my name is ${me.name}.`;
+    }
+}
+
+// console.log(me.name); // 'Kat'
+// me.getGreeting(); // => 'Hi, my name is Kat.'
