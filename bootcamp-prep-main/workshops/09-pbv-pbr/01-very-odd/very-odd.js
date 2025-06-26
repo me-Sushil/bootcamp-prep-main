d4b9eac4887359ac85281b23cbef97da2b24a63f
me-Sushil <girbahadurbk123@gmail.com>
2025-06-26 23:31:02 +0545

    PBVandPBR

--- bootcamp-prep-main/workshops/09-pbv-pbr/01-very-odd/very-odd.js
+++ bootcamp-prep-main/workshops/09-pbv-pbr/01-very-odd/very-odd.js
@@ -1,1 +1,22 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+// this is pass by value example
+
+// function changeValue(num) {
+//   num = 100;
+//   console.log("Inside function:", num); // 100
+// }
+
+// let x = 50;
+// changeValue(x);
+// console.log("Outside function:", x); // 50
+
+
+//This is pass by reference example
+// function addItem(arr) {
+//   arr.push("Momo");
+//   console.log("Inside function:", arr); // ["Pizza", "Burger", "Momo"]
+// }
+
+// let food = ["Pizza", "Burger"];
+// addItem(food);
+// console.log("Outside function:", food); // ["Pizza", "Burger", "Momo"]
