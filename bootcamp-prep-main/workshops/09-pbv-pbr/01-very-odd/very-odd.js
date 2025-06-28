de84de91bf963148254b39f8e86b42969223214e
me-Sushil <girbahadurbk123@gmail.com>
2025-06-28 20:03:11 +0545

    very-odd commit

--- bootcamp-prep-main/workshops/09-pbv-pbr/01-very-odd/very-odd.js
+++ bootcamp-prep-main/workshops/09-pbv-pbr/01-very-odd/very-odd.js
@@ -20,3 +20,21 @@
 // let food = ["Pizza", "Burger"];
 // addItem(food);
 // console.log("Outside function:", food); // ["Pizza", "Burger", "Momo"]
+
+
+function veryOdd(arr){
+    let oddNums = [];
+    for(let i=0; i<arr.length; i++){
+        let num = arr[i];
+        if(num % 2 === 1){
+            oddNums.push(num);
+        }
+    }
+    return oddNums;
+}
+
+// let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
+// let oddNums = veryOdd(allTheNums);
+
+// console.log('oddNums:', oddNums); // [1, 3, 5, 7];
+// console.log('allTheNums:', allTheNums); // [1, 2, 3, 4, 5, 6, 7, 8]
