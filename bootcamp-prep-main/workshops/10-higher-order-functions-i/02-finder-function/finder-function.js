eec31a2d93b32288b09553534da7ba619ea92ead
me-Sushil <girbahadurbk123@gmail.com>
2025-07-10 00:03:42 +0545

    update

--- bootcamp-prep-main/workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
+++ bootcamp-prep-main/workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,11 +1,12 @@
 function finderFunction(arr, callback) {
-  for (let i = 0; i < arr.length; ++i) {
+  for (let i = 0; i < arr.length; i++) {
     let element = arr[i];
 
     let foundit = callback(element);
     if (foundit) {
-      return true;
+      return i;
+    }
     }
     return -1;
-  }
+  
 }
--- bootcamp-prep-main/workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ bootcamp-prep-main/workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function whosASpecial(arr){
+    let result = '';
+
+    arr.forEach(({name, species}, idx)=>{
+    result += `${name} the ${species} is very special!`;
+
+    if(idx !== arr.length -1){
+        result += " ";
+    }
+    })
+    return result;
+}
