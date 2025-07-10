26b6a098abd4bc871a70afc7f0763ff5f819ce69
me-Sushil <girbahadurbk123@gmail.com>
2025-07-10 19:10:04 +0545

    get dog breeder commit

--- bootcamp-prep-main/workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
+++ bootcamp-prep-main/workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+const getDogBreeder = (defaultname, defaultage)=>{
+    return (name = defaultname, age=defaultage)=>{
+    if(typeof name === 'number'){
+        age = name;
+        name = defaultname;
+    }
+    let newDog={
+        name : name,
+        age : age,
+    }
+    return newDog;
+}   
+}
