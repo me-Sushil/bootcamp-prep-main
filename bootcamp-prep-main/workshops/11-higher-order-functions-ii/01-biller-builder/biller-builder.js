64d5ce1cf10c93acb2b47dfdd380c10ee9dbac60
me-Sushil <girbahadurbk123@gmail.com>
2025-07-10 14:07:31 +0545

    biller-builder commit

--- bootcamp-prep-main/workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
+++ bootcamp-prep-main/workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
@@ -1,1 +1,29 @@
-// YOUR CODE BELOW
+// function billerBuilder(nameOfState){
+
+//     return function(priceOfItem){
+//         if(nameOfState === 'NY'){
+//           return  priceOfItem * 1.03 * 1.04;
+//         }
+//         if(nameOfState === "NJ"){
+//           return priceOfItem * 1.05 * 1.06625;
+//         }
+//     }
+
+// }
+
+
+function billerBuilder(state){
+
+    if(state === "NY"){
+        return applyNYTexes;
+    }
+    return applyNJTexes;
+
+    function applyNYTexes(price){
+        return price * 1.03 * 1.04;
+    }
+
+    function applyNJTexes(price){
+        return price * 1.05 * 1.06625;
+    }
+}
