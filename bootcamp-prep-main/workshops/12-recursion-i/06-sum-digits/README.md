2b40e70e88345e3a12f3f1ffcec71b82db1ca8f3
me-Sushil <girbahadurbk123@gmail.com>
2025-07-11 20:34:51 +0545

    sum digit commit

--- bootcamp-prep-main/workshops/12-recursion-i/06-sum-digits/README.md
+++ bootcamp-prep-main/workshops/12-recursion-i/06-sum-digits/README.md
@@ -1,7 +1,6 @@
 ### Sum Digits
 
-Write a function, sumDigits, that sums all the digits of a given integer
-(assume positive). Use recursion.
+Write a function, sumDigits, that sums all the digits of a given integer (assume positive). Use recursion.
 
 ```javascript
 sumDigits(1234) // => 10
--- bootcamp-prep-main/workshops/12-recursion-i/06-sum-digits/sum-digits.js
+++ bootcamp-prep-main/workshops/12-recursion-i/06-sum-digits/sum-digits.js
@@ -1,1 +1,10 @@
-// YOUR CODE BELOW
+const sumDigits = (aNum)=>{
+    let aString = aNum.toString();
+
+    if(aString.length === 1){
+        return parseInt(aString);
+    }
+    let restDigit = aString.slice(1);
+    let restNum = parseInt(restDigit);
+    return parseInt(aString[0]) + sumDigits(restNum);
+}
