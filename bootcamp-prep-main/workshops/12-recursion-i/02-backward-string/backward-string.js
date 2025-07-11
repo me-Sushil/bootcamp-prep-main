4854cfc2f610379fff5d36b0e09ed5045e4781c4
me-Sushil <girbahadurbk123@gmail.com>
2025-07-11 10:36:37 +0545

    backward string commit

--- bootcamp-prep-main/workshops/12-recursion-i/02-backward-string/backward-string.js
+++ bootcamp-prep-main/workshops/12-recursion-i/02-backward-string/backward-string.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+const backwardString = (aStr)=>{
+   if(aStr.length === 1){
+    console.log(aStr);
+   }else{
+    let lastChar = aStr[aStr.length-1];
+    console.log(lastChar);
+    backwardString(aStr.slice(0, -1));
+   }
+}
