e4d0cff8c2087fdd09443207352a6491d628e4d6
me-Sushil <girbahadurbk123@gmail.com>
2025-07-11 17:11:19 +0545

    reverse array commit

--- bootcamp-prep-main/workshops/12-recursion-i/05-reverse-array/reverse-array.js
+++ bootcamp-prep-main/workshops/12-recursion-i/05-reverse-array/reverse-array.js
@@ -1,1 +1,7 @@
-// YOUR CODE BELOW
+const reverseArray =(array)=>{
+    if(array.length === 1){
+        return array;
+    }
+    let lastArry = array[array.length -1];
+    return [lastArry].concat(reverseArray(array.slice(0, -1)));
+}
