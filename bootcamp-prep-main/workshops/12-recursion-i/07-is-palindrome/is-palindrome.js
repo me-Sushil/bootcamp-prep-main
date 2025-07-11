a5a6efc6ed90e34573036e198be07ece189eca53
me-Sushil <girbahadurbk123@gmail.com>
2025-07-11 21:52:20 +0545

    is-Palindrome commit

--- bootcamp-prep-main/workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
+++ bootcamp-prep-main/workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
@@ -1,1 +1,13 @@
-// YOUR CODE BELOW
+const isPalindrome = (aStr) =>{
+    if(aStr.length <= 1){
+        return true;
+    }
+    let firstChar = aStr[0].toLowerCase();
+    let lastChar = aStr[aStr.length - 1].toLowerCase();
+
+    if(firstChar === lastChar){
+        return isPalindrome(aStr.slice(1, -1));
+    }else{
+        return false;
+    }
+}
